61e8ea8e4cdf27c6854d447fa5571e1fd21eeb17
Anil Kumar Singh <aiklavya@gmail.com>
2024-11-27 22:13:28 +0530

    Initial commit of Bhaashik on GitHub as a NetBeans project.

---
+++ src/main/java/bhaashik/corpus/span/SpanAnnotationStoryImpl.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bhaashik.corpus.span;
+
+import bhaashik.corpus.ssf.impl.SSFStoryImpl;
+
+/**
+ *
+ * @author Anil Kumar Singh
+ */
+public class SpanAnnotationStoryImpl extends SSFStoryImpl implements SpanAnnotationStory {
+
+}
